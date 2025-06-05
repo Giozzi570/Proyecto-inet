a3375924485608058e1b5e55bb0a9134425adc3e
Federico Giozzi <fedegiozzi@gmail.com>
2025-06-05 18:31:34 -0300

    cambios-2

--- src/app/components/header_footer/header.tsx
+++ src/app/components/header_footer/header.tsx
@@ -2,15 +2,19 @@
 
 import React, { useState } from 'react';
 import "./header.css";
-const style = "text-[#111117] rounded-4xl font-medium bg-white py-2 px-4"
+import Gente from './sections_information/gente';
+
+const stylePasive = "text-white cursor-pointer rounded-4xl font-medium py-2 px-4"
+const styleActive = "text-[#111117] cursor-pointer rounded-4xl bg-white font-medium py-2 px-4"
 const items = [
-  { text: "Gente", style: style },
-  { text: "Gente", style: style },
-  { text: "Gente", style: style },
-  { text: "Gente", style: style }
+  { text: "Gente", style: styleActive },
+  { text: "Gente", style: stylePasive },
+  { text: "Gente", style: stylePasive },
+  { text: "Gente", style: stylePasive }
 ];
 function Header() {
-  const [active, setActive] = useState(false);
+   const [position, setPosition] = useState('translate(100%)');
+   const [filter, setFilter] = useState('blur(0px)');
 
   return (
     <> 
@@ -18,13 +22,16 @@ function Header() {
         <button
         id='button-nav'
         data-collapse-toggle="navbar-multi-level"
-        onClick={() => setActive(prev => !prev)}
+        onClick={() => (setPosition(position === 'translate(100%)' ? 'translate(0%)' : 'translate(100%)'), setFilter(filter === 'blur(0px)' ? 'blur(18px)' : 'blur(0px)'))}
+        
         type="button"
-        className="cursor-pointer"
+        className="flex items-center justify-between w-8 py-2 px-3 text-gray-900 rounded-sm md:hover:bg-transparent md:border-0 md:p-0 md:w-12 h-12 cursor-pointer
+        "
         aria-controls="navbar-multi-level"
         aria-expanded="false"
       >
         <svg
+          id='svg-nav'
           className="w-5 h-5"
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
@@ -40,8 +47,13 @@ function Header() {
           />
         </svg>
       </button>
-      <div id='header-middle' className='w-4/6 bg-deep-koamaru-950 h-screen'>
-            <div className='flex column'>
+      <div className='w-full h-screen flex' style= {{ transform: position }} id='container-header'>
+        <div id='content-middle' className='w-1/3
+ h-screen'>
+           <div id='background'></div>
+           <img id='image-background' src="https://r-charts.com/es/miscelanea/procesamiento-imagenes-magick_files/figure-html/color-fondo-imagen-r.png" alt="" />
+        </div>
+        <div id='header-middle' className='w-4/6 h-screen flex flex-col'>
                 <nav className='w-full p-6'>
                     <header>
                         <ul className='flex justify-around gap-5'>
@@ -50,14 +62,13 @@ function Header() {
                                     {item.text}
                                 </li>
                             ) )}
-                            
                         </ul>
                     </header>
                 </nav>
-                <div></div>
+                <Gente></Gente>
             </div>
         </div>
-    </div>
+      </div>
         
     </>
   );
---
+++ src/app/components/header_footer/sections_information/gente.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React, { useState } from 'react';
+import "./gente.css"
+const stylePasive = "text-white cursor-pointer rounded-4xl font-medium py-2 px-4"
+const items = [
+  { text: "Jorge Federico Giozzi", style: stylePasive },
+  { text: "Luca Gambini", style: stylePasive },
+  { text: "Gonzalo Martín Dettler", style: stylePasive },
+  { text: "Adriel Benjamín Acosta", style: stylePasive }
+];
+function Gente(){
+    return (
+        <div className='text-white h-full flex py-10 mx-4'>
+                    <section>
+                        <ul id='gente-container'>
+                            {items.map((item,index) => (
+                                <li key={index} className={item.style}>
+                                    {item.text}
+                                </li>
+                            ) )}
+                        </ul>
+                    </section>
+                </div>
+    )
+}
+export default Gente
